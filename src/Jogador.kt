a264caee786692e2d56a03e39dc6292f0c44278c
iYzor <nlxvini420@gmail.com>
2025-02-24 18:45:14 -0300

    ClasseJogadorCriada

---
+++ src/Jogador.kt
@@ -0,0 +1,12 @@
+class Jogador {
+
+    fun pedirNomes(): Pair<String, String> {
+        println("Digite o nome do Jogador 1: ")
+        val jogador1 = readln()
+        println("Digite o nome do Jogador 2: ")
+        val jogador2 = readln()
+
+        return Pair(jogador1, jogador2)
+    }
+
+}
